d7678fb542a27ac2b82e6846d3ad8529286b016d
Jerry Zhang <jerryzhang.dev@gmail.com>
2024-04-08 17:34:16 -0400

    Update streamlit_app.py
    show information of each fruit

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,8 +26,10 @@ if ingredients_list:
 
     for fruit in ingredients_list:
         ingredients_string += fruit + ' '
-
-    # st.write(ingredients_string)
+    
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit)
+        st.subheader(fruit + ' Nutrition Information')
+        fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
 
     my_insert_stmt = """
         insert into smoothies.public.orders(ingredients, name_on_order)
@@ -43,7 +45,4 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
 
         st.success('Your Smoothie is ordered, '+ name_on_order +'!', icon="✅")
-
-    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-    st.text(fruityvice_response.json())
 
